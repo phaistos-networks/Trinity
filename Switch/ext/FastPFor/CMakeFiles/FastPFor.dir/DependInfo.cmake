a82b4c463ed1f066229e0a52ec76dc8dc5762c03
Mark Papadakis <mpapadakis@phaistosnetworks.gr>
2017-05-09 14:01:31 +0300

    Added Switch/ext

---
+++ Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/system/Development/Switch/ext/FastPFor/src/streamvbyte.c" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/streamvbyte.c.o"
+  "/home/system/Development/Switch/ext/FastPFor/src/varintdecode.c" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/varintdecode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "headers"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/system/Development/Switch/ext/FastPFor/src/bitpacking.cpp" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/bitpacking.cpp.o"
+  "/home/system/Development/Switch/ext/FastPFor/src/bitpackingaligned.cpp" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/bitpackingaligned.cpp.o"
+  "/home/system/Development/Switch/ext/FastPFor/src/bitpackingunaligned.cpp" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/bitpackingunaligned.cpp.o"
+  "/home/system/Development/Switch/ext/FastPFor/src/horizontalbitpacking.cpp" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/horizontalbitpacking.cpp.o"
+  "/home/system/Development/Switch/ext/FastPFor/src/simdbitpacking.cpp" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/simdbitpacking.cpp.o"
+  "/home/system/Development/Switch/ext/FastPFor/src/simdunalignedbitpacking.cpp" "/home/system/Development/Switch/ext/FastPFor/CMakeFiles/FastPFor.dir/src/simdunalignedbitpacking.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
